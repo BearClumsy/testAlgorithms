0b266d0e0a2684f0dce55a1a2982edc146c5cac4
Platonov, Dmitriy <dmytro_platononv1@epam.com>
2025-03-31 13:46:39 +0300

    reformat code

--- src/dplatonov/Fibonacci.java
+++ src/dplatonov/Fibonacci.java
@@ -1,32 +1,32 @@
 package dplatonov;
 
 public class Fibonacci {
-  public static void main(String[] args) {
-    int n1 = 1, n2 = 1, size = 11;
-    int[] result = new int[size];
-    for (int i = 0; i < size; i++) {
-      if (i < 2) {
-        result[i] = n1;
-      } else {
-        result[i] = n1 + n2;
-        n1 = n2;
-        n2 = result[i];
-      }
+    public static void main(String[] args) {
+        int n1 = 1, n2 = 1, size = 11;
+        int[] result = new int[size];
+        for (int i = 0; i < size; i++) {
+            if (i < 2) {
+                result[i] = n1;
+            } else {
+                result[i] = n1 + n2;
+                n1 = n2;
+                n2 = result[i];
+            }
+        }
+
+        print(result);
     }
 
-    print(result);
-  }
-
-  public static void print(int[] a) {
-    int size = a.length;
-    System.out.print("dplatonov.Fibonacci of 11 = {");
-    for (int i = 0; i < size; i++) {
-      System.out.print(a[i]);
-      if (i != size - 1) {
-        System.out.print(", ");
-      } else {
-        System.out.print("}");
-      }
+    public static void print(int[] a) {
+        int size = a.length;
+        System.out.print("dplatonov.Fibonacci of 11 = {");
+        for (int i = 0; i < size; i++) {
+            System.out.print(a[i]);
+            if (i != size - 1) {
+                System.out.print(", ");
+            } else {
+                System.out.print("}");
+            }
+        }
     }
-  }
 }
